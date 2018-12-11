a41a1d6fc7a8cb76112c047ca74217ebd519c0ac
WANCOLIN <949927078@qq.com.com>
2018-12-11 19:12:28 +0800

    添加入口文件

---
+++ basic/index.php
@@ -0,0 +1,13 @@
+<?php
+//comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
+
+//判断加载的项目（web、wap、api、web）
+if (strpos($_SERVER['PHP_SELF'], '/wap/') !== false)
+    header('Location:/wap');
+else
+    header('Location:/web');
